c80eadc951561bebaa932f4b17db79c5d6c8a0c6
omkarstpt <omkarstpt@gmail.com>
2018-11-26 00:07:52 -0600

    Removed template code from settings and link screens

--- screens/LinksScreen.js
+++ screens/LinksScreen.js
@@ -1,18 +1,17 @@
-import React from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
-import { ExpoLinksView } from '@expo/samples';
+import React from "react";
+import { ScrollView, StyleSheet, Text } from "react-native";
 
 export default class LinksScreen extends React.Component {
   static navigationOptions = {
-    title: 'Links',
+    title: "Categories"
   };
 
   render() {
     return (
       <ScrollView style={styles.container}>
         {/* Go ahead and delete ExpoLinksView and replace it with your
-           * content, we just wanted to provide you with some helpful links */}
-        <ExpoLinksView />
+         * content, we just wanted to provide you with some helpful links */}
+        <Text style={styles.getStartedText}>Categories!</Text>
       </ScrollView>
     );
   }
@@ -22,6 +21,12 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingTop: 15,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff"
   },
+  getStartedText: {
+    fontSize: 17,
+    color: "rgba(96,100,109, 1)",
+    lineHeight: 24,
+    textAlign: "center"
+  }
 });
